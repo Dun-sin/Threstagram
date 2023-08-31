d521410d87ff6e2ffe1352c1c45851111122a048
Dun-sin <jesudunsinfaiye@gmail.com>
2023-08-31 12:28:53 +0100

    fix: get the base64 string of the image

--- src/pages/api/threads/profile/[username].ts
+++ src/pages/api/threads/profile/[username].ts
@@ -16,7 +16,18 @@ export default async function handler(
 			return;
 		}
 		const user = await threadsAPI.getUserProfile(userID);
-		res.status(200).json({ isSucess: true, message: user.profile_pic_url });
+
+		const url = user.profile_pic_url;
+		const response = await fetch(url);
+
+		const blob = await response.arrayBuffer();
+		const contentType = response.headers.get('content-type');
+
+		const base64String = `data:${contentType};base64,${Buffer.from(
+			blob,
+		).toString('base64')}`;
+
+		res.status(200).json({ isSucess: true, message: base64String });
 	} else {
 		res
 			.status(500)
