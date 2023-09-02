4369d145df4ae85b3c6173be660b5df8cfa7d954
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-09-02 09:37:54 +0100

    fix(post): text overflows when the length of the text is large (#21)
    fix #5

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import { InfinitySpin } from 'react-loader-spinner';
 
-import { elementToImage } from '../utils/helper';
+import { calculateFontSize, elementToImage } from '../utils/helper';
 import Image from 'next/image';
 
 type User = {
@@ -43,34 +43,38 @@ const Preview = (props: PreviewProps) => {
 				className={`flex gap-4 w-full snap-x snap-mandatory overflow-x-scroll h-auto ${
 					postContent.length === 1 && 'justify-center'
 				}`}>
-				{postContent.map((content: string, index: number) => (
-					<div
-						className={`min-h-[337.5px] max-h-[337.5px] h-[337.5px] min-w-[270px] max-w-[270px] w-[270px] flex justify-between flex-col rounded-md p-4 snap-center instagram-${index} overflow-y-scroll`}
-						key={index}
-						style={{ backgroundColor: color, fontFamily }}
-						id='card-container'>
-						{postContent.length !== 1 && (
-							<span className='text-fmd font-semibold border-b w-full border-secondary border-spacing-5 h-[10%]'>
-								{index}
+				{postContent.map((content: string, index: number) => {
+					return (
+						<div
+							className={`min-h-[337.5px] max-h-[337.5px] h-[337.5px] min-w-[270px] max-w-[270px] w-[270px] flex justify-between flex-col rounded-md p-4 snap-center instagram-${index} overflow-y-scroll`}
+							key={index}
+							style={{ backgroundColor: color, fontFamily }}
+							id='card-container'>
+							{postContent.length !== 1 && (
+								<span className='text-fmd font-semibold border-b w-full border-secondary border-spacing-5 h-[10%]'>
+									{index}
+								</span>
+							)}
+							<p
+								className={`h-[80%] flex items-center`}
+								style={{ fontSize: calculateFontSize(content) }}>
+								{content}
+							</p>
+							<span className='flex gap-2 items-center border-t border-secondary border-spacing-5 h-[10%]'>
+								<div className='h-8 w-8 rounded-full relative overflow-hidden border-2 border-secondary  border-spacing-6'>
+									<Image
+										src={postUser.avatar}
+										alt={postUser.username}
+										quality={100}
+										fill
+										className='object-cover'
+									/>
+								</div>
+								<p className='text-fxs font-medium'>@{postUser.username}</p>
 							</span>
-						)}
-						<span className={`text-f2xs h-[80%] flex items-center`}>
-							{content}
-						</span>
-						<span className='flex gap-2 items-center border-t border-secondary border-spacing-5 h-[10%]'>
-							<div className='h-8 w-8 rounded-full relative overflow-hidden border-2 border-secondary  border-spacing-6'>
-								<Image
-									src={postUser.avatar}
-									alt={postUser.username}
-									quality={100}
-									fill
-									className='object-cover'
-								/>
-							</div>
-							<p className='text-fxs font-medium'>@{postUser.username}</p>
-						</span>
-					</div>
-				))}
+						</div>
+					);
+				})}
 			</section>
 			<button
 				className='bg-brand rounded-md h-16 p-4 font-medium w-[270px] flex items-center justify-center'
