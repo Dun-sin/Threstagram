789326845c0406fde61f248dd939557edf8bd57d
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-09-04 22:20:26 +0100

    feat(seo): use next SEO properly (#44)

---
+++ next-seo.config.js
@@ -0,0 +1,24 @@
+export default {
+  title: 'Threstagram - Threads to Images',
+  defaultTitle: 'Threstagram - Threads to Images',
+  description: 'Convert Your Threads Posts to Images',
+  openGraph: {
+    type: 'website',
+    url: 'https://threstagram.vercel.app',
+    title: 'Threstagram - Threads to Images',
+    description: 'Convert Your Threads Posts to Images',
+    images: [
+      {
+        url: 'https://github-production-user-asset-6210df.s3.amazonaws.com/78784850/263960112-c1cb2dba-2379-4aa4-aba1-5ac124f7bcae.png',
+        alt: 'Threstagram Alt',
+        height: 600,
+        width: 800,
+      },
+    ],
+    'site-name': 'Threstagram',
+  },
+  twitter: {
+    handle: '@dunsincodes',
+    cardType: 'summary_large_image',
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,47 +41,28 @@ export default function Home() {
   }, [userState]);
 
   return (
-    <>
-      <NextSeo
-        openGraph={{
-          type: 'website',
-          url: 'https://threstagram.vercel.app',
-          title: 'Threstagram',
-          description: 'Convert Your Threads Posts to Images',
-          images: [
-            {
-              url: 'https://github-production-user-asset-6210df.s3.amazonaws.com/78784850/263960112-c1cb2dba-2379-4aa4-aba1-5ac124f7bcae.png',
-              alt: 'Threstagram Alt',
-              height: 600,
-              width: 800,
-            },
-          ],
-        }}
-      />
+    <section className='bg-primary text-secondary flex items-center flex-col justify-center md:h-screen min-h-screen w-screen gap-10 pt-5 pb-10 font-sans'>
+      <Social />
+      <main className='flex items-center w-full flex-col justify-center gap-4'>
+        <h1 className='md:text-flg text-fmd font-bold text-center'>
+          Convert Your Threads Post To Images
+        </h1>
+        {/* Input */}
+        <Options setPostURL={setPostURL} />
 
-      <section className='bg-primary text-secondary flex items-center flex-col justify-center md:h-screen min-h-screen w-screen gap-10 pt-5 pb-10 font-sans'>
-        <Social />
-        <main className='flex items-center w-full flex-col justify-center gap-4'>
-          <h1 className='md:text-flg text-fmd font-bold text-center'>
-            Convert Your Threads Post To Images
-          </h1>
-          {/* Input */}
-          <Options setPostURL={setPostURL} />
-
-          {contentState.contentLoading ? (
-            <span className='flex items-center justify-center w-4/5 max-w-[850px]'>
-              <PongSpinner
-                size={110}
-                color='#fff'
-                loading={contentState.contentLoading}
-              />
-            </span>
-          ) : (
-            // Display Content
-            !(contentState.postContent.length === 0) && <Preview />
-          )}
-        </main>
-      </section>
-    </>
+        {contentState.contentLoading ? (
+          <span className='flex items-center justify-center w-4/5 max-w-[850px]'>
+            <PongSpinner
+              size={110}
+              color='#fff'
+              loading={contentState.contentLoading}
+            />
+          </span>
+        ) : (
+          // Display Content
+          !(contentState.postContent.length === 0) && <Preview />
+        )}
+      </main>
+    </section>
   );
 }
