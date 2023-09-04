4eb0882a7d367b6d8cf1c772b504ca460940c9d7
Dunsin <jesudunsinfaiye@gmail.com>
2023-09-04 21:21:35 +0100

    build: find error in error

--- src/pages/api/threads/profile/[username].ts
+++ src/pages/api/threads/profile/[username].ts
@@ -5,32 +5,33 @@ import { ThreadsAPI } from 'threads-api';
 const threadsAPI = new ThreadsAPI();
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-	if (req.method === 'GET') {
-		const username = req.query.username as string;
+  console.log('debugging production', req.method);
+  if (req.method === 'GET') {
+    const username = req.query.username as string;
 
-		const userID = await threadsAPI.getUserIDfromUsername(username);
-		if (!userID) {
-			return;
-		}
-		const user = await threadsAPI.getUserProfile(userID);
+    const userID = await threadsAPI.getUserIDfromUsername(username);
+    if (!userID) {
+      return;
+    }
+    const user = await threadsAPI.getUserProfile(userID);
 
-		const url = user.profile_pic_url;
-		const response = await fetch(url);
+    const url = user.profile_pic_url;
+    const response = await fetch(url);
 
-		const blob = await response.arrayBuffer();
-		const contentType = response.headers.get('content-type');
+    const blob = await response.arrayBuffer();
+    const contentType = response.headers.get('content-type');
 
-		const base64String = `data:${contentType};base64,${Buffer.from(
-			blob,
-		).toString('base64')}`;
+    const base64String = `data:${contentType};base64,${Buffer.from(
+      blob
+    ).toString('base64')}`;
 
-		res.status(200).json({ isSucess: true, message: base64String });
-	} else {
-		res
-			.status(500)
-			.json({ isSuccess: false, message: 'Internal Server Error' });
-	}
+    res.status(200).json({ isSucess: true, message: base64String });
+  } else {
+    res
+      .status(500)
+      .json({ isSuccess: false, message: 'Internal Server Error' });
+  }
 }
