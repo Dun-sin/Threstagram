487fedd6267f79fd8d115dcbeca0fa3eacee1582
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-09-04 21:51:16 +0100

    fix(font): set the default fonts properly (#43)
    * fix(font): set the default for the selectedFont
    
    fix #26
    
    * build: find error in error
    
    * fix(font): general font is now set properly
    
    * fix(font): turning black and giving an error
    
    * fix(font): missing double quotes in fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: '#060b0a',
-				secondary: '#f9ffff',
-				brand: '#ff4847',
-			},
-			fontSize: {
-				f2xs: 'var(--step--2)',
-				fxs: 'var(--step--1)',
-				fsm: 'var(--step-0)',
-				fmd: 'var(--step-1)',
-				flg: 'var(--step-2)',
-				fxl: 'var(--step-3)',
-				f2xl: 'var(--step-4)',
-				f3xl: 'var(--step-5)',
-			},
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Exo 2"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#060b0a',
+        secondary: '#f9ffff',
+        brand: '#ff4847',
+      },
+      fontSize: {
+        f2xs: 'var(--step--2)',
+        fxs: 'var(--step--1)',
+        fsm: 'var(--step-0)',
+        fmd: 'var(--step-1)',
+        flg: 'var(--step-2)',
+        fxl: 'var(--step-3)',
+        f2xl: 'var(--step-4)',
+        f3xl: 'var(--step-5)',
+      },
+    },
+  },
+  plugins: [],
 };
