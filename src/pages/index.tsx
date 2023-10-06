d89af07a982a0e57660595d0a5902bed8da9f7f3
Divyesh Khuva <khuvadivyesh15@gmail.com>
2023-10-06 15:17:30 +0530

    fixed homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,10 +12,10 @@ export default function Home() {
       />
       <div className='flex items-center w-full flex-col justify-center overflow-scroll bg-white-400 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-10 h-full pb-10 gap-4'>
         <h1 className='font-bold text-f3xl'>Threstagram</h1>
-        <h2 className='font-semibold text-f2xl -mt-7'>
+        <h2 className='font-semibold text-f2xl text-center -mt-7'>
           Welcome to the social toolbox
         </h2>
-        <main className='flex gap-5'>
+        <main className='flex gap-5 flex-col sm:flex-row'>
           <Link href='/threads'>
             <section className='h-52 w-64 text-fmd text-center cursor-pointer border border-secondary flex items-center justify-center rounded px-2'>
               <span>Convert Your Threads Post to Image</span>
